bf95836d140e78c8eb728cf932a2e5f87b2e2bec
chikanara <usertestabc35@gmail.com>
2021-05-19 12:20:18 +0100

    first commit

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const ProductDetail = ({ products, match }) => {
+  const product = products.find((el) => el.id.toString() === match.params.id);
+  console.log(match);
+  console.log(product);
+
+  return (
+    <Card style={{ width: '22rem' }}>
+      <Card.Img variant="top" src={product.imgSrc} />
+      <Card.Body>
+        <Card.Title>{product.name}</Card.Title>
+        <Card.Text>{product.price}</Card.Text>
+        <Link to="/products">
+          <Button variant="primary">Go Back</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductDetail;
